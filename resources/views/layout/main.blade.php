43b3db6f3650e548f33e312593c30569005c5ed9
jose.developer <jose.developer@gmail.com>
2020-03-21 20:06:14 -0300

    primer commit

---
+++ resources/views/layout/main.blade.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+    @include('layout.head')
+
+</head>
+
+<body id="page-top">
+    
+    <!-- Page Wrapper -->
+    <div id="wrapper">
+
+        @include('layout.sidebar')
+
+        <!-- Content Wrapper -->
+        <div id="content-wrapper" class="d-flex flex-column">
+
+            <!-- Main Content -->
+            <div id="content">
+
+                @include('layout.navbar')
+
+                <!-- AQUI VA EL CONTENIDO PRINCIPAL -->
+                <div class="container-fluid">
+
+                    @yield('content')
+
+                </div>
+                <!-- /.container-fluid -->
+
+            </div>
+            <!-- End of Main Content -->
+
+            @include('layout.footer')
+
+        </div>
+        <!-- End of Content Wrapper -->
+
+    </div>
+    <!-- End of Page Wrapper -->
+
+    @include('layout.js')
+
+</body>
+</html>
