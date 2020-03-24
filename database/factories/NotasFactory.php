df4530b34a605e29f91946fa1e6e6232d153b9df
jose.developer <jose.developer@gmail.com>
2020-03-24 04:15:06 -0300

    cambios

---
+++ database/factories/NotasFactory.php
@@ -0,0 +1,14 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Nota;
+use Faker\Generator as Faker;
+
+$factory->define(Nota::class, function (Faker $faker) {
+    return [
+        'descripcion' => $faker->sentence,
+        'urgencia' => $faker->name,
+    ];
+});
+
--- database/seeds/Inicializar.php
+++ database/seeds/Inicializar.php
@@ -8,37 +8,6 @@ class Inicializar extends Seeder
 {
     public function run(){
 
-        // crear notas ficticias
-        $nota = new Nota();
-        $nota->descripcion = 'descripcion nota 1, solo para ejemplo';
-        $nota->urgencia = 'alta';
-        $nota->save();
-
-        $nota = new Nota();
-        $nota->descripcion = 'descripcion nota 2, solo para ejemplo';
-        $nota->urgencia = 'alta';
-        $nota->save();
-
-        $nota = new Nota();
-        $nota->descripcion = 'descripcion nota 3, solo para ejemplo';
-        $nota->urgencia = 'alta';
-        $nota->save();
-
-        $nota = new Nota();
-        $nota->descripcion = 'descripcion nota 4, solo para ejemplo';
-        $nota->urgencia = 'baja';
-        $nota->save();
-        
-        $nota = new Nota();
-        $nota->descripcion = 'descripcion nota 5, solo para ejemplo';
-        $nota->urgencia = 'baja';
-        $nota->terminado = 1;
-        $nota->save();
-
-        $nota = new Nota();
-        $nota->descripcion = 'descripcion nota 5, solo para ejemplo';
-        $nota->urgencia = 'baja';
-        $nota->terminado = 1;
-        $nota->save();
+        factory(Nota::class, 4)->create();
     }
 }
